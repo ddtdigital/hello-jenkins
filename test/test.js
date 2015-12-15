24723ab2c9575f951a95af025188b7936c50111c
ddtdigital <dtagert@gmail.com>
2015-12-15 16:56:03 -0600

    testing push triggering jenkins

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-    it('responds with what up people', function(done) {
-        request(app).get('/').expect('what up people ', done);
+    it('responds with what up peeps', function(done) {
+        request(app).get('/').expect('what up peeps', done);
     });
 });
