fb09f9ee0888a414323d65a06a652782f357c3a5
ddtdigital <dtagert@gmail.com>
2015-12-15 14:59:11 -0600

    trigger not working due to git username?

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
     it('responds with what up people', function(done) {
-        request(app).get('/').expect('what up people', done);
+        request(app).get('/').expect('what up people ', done);
     });
 });
