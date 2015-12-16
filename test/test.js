8402068334692e04c1e1b05f0d01d7b0788dc596
ddtdigital <dtagert@gmail.com>
2015-12-15 20:00:18 -0600

    peeps to peoples

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
     it('responds with what up peeps. hopefully last test.', function(done) {
-        request(app).get('/').expect('what up peeps. hopefully last test.', done);
+        request(app).get('/').expect('what up peoples. hopefully last test.', done);
     });
 });
